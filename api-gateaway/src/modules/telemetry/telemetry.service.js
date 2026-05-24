162da66ad78d5cff35b1afdea6fc1e06e859d684
Andika Palian <andika12des@gmail.com>
2026-05-24 18:17:54 +0800

    feat: remove anomaly detection logic and add AI alert listener

--- api-gateaway/src/modules/telemetry/telemetry.service.js
+++ api-gateaway/src/modules/telemetry/telemetry.service.js
@@ -4,64 +4,7 @@ import { publishToQueue } from "../../services/rabbitmq.service.js";
 import * as alertService from "../alerts/alert.service.js";
 import logger from "../../utils/logger.js";
 
-// Safety net to detect anomalies
-const analyzeAnomalies = async (telemetryData) => {
-  const { equipmentId, metrics } = telemetryData;
-
-  if (!metrics) return;
-
-  if (metrics.temperature) {
-    if (metrics.temperature >= 100) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `DANGEROUS: Temperature is dangerously high (${metrics.temperature}°C). Overheat risk!`,
-        severity: "CRITICAL",
-      });
-    } else if (metrics.temperature >= 85) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `WARNING: Temperature is going dangerously high (${metrics.temperature}°C). Overheat risk!`,
-        severity: "WARNING",
-      });
-    }
-  }
-
-  if (metrics.vibration) {
-    if (metrics.vibration >= 20) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `DANGEROUS: Vibration is dangerously high (${metrics.vibration} mm/s). Stuctural failure imminent!`,
-        severity: "CRITICAL",
-      });
-    } else if (metrics.vibration >= 15) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `WARNING: Vibration is going dangerously high (${metrics.vibration} mm/s).`,
-        severity: "WARNING",
-      });
-    }
-  }
-
-  if (metrics.rpm !== undefined && metrics.rpm !== null) {
-    if (metrics.rpm >= 3600) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `DANGEROUS: RPM is dangerously high (${metrics.rpm} RPM). Risk of catastrophic failure!`,
-        severity: "CRITICAL",
-      });
-    } else if (metrics.rpm <= 500 && metrics.rpm > 0) {
-      await alertService.createAlert({
-        equipmentId: equipmentId,
-        message: `WARNING: Engine RPM is dropping abnormally (${metrics.rpm} RPM). Possible load stall.`,
-        severity: "WARNING",
-      });
-    }
-  }
-};
-
 export const processAndSaveTelemetry = async (telemetryData) => {
-  // let currentSyncStatus = "PENDING";
-  // let rabbitMqError = null;
   const newTelemetry = await Telemetry.create({
     equipmentId: telemetryData.equipmentId,
     timestamp: telemetryData.timestamp || new Date(),
@@ -73,9 +16,6 @@ export const processAndSaveTelemetry = async (telemetryData) => {
   try {
     // Try to send data to RabbitMQ
     await publishToQueue("sensor_data", telemetryData);
-
-    // If successful, update syncStatus to SYNCED
-    // currentSyncStatus = "SYNCED";
   } catch (error) {
     // If there's an error sending to RabbitMQ, update syncStatus to FAILED
     // currentSyncStatus = "FAILED";
@@ -84,21 +24,6 @@ export const processAndSaveTelemetry = async (telemetryData) => {
 
     await TelemetryOutbox.create({ payload: newTelemetry });
   }
-
-  // if (currentSyncStatus === "SYNCED") {
-  //   logger.info(
-  //     `[TELEMETRY SERVICE] Data saved and sent to RabbitMQ successfully. ID: ${newTelemetry._id}`,
-  //   );
-  // } else {
-  //   logger.warn(
-  //     `[TELEMETRY SERVICE] Data saved but RabbitMQ failed. ID: ${newTelemetry._id} | Error: ${rabbitMqError}`,
-  //   );
-  // }
-
-  // Fallback anomaly detection
-  analyzeAnomalies(telemetryData).catch((err) => {
-    logger.error(`[TELEMETRY SERVICE] Fallback anomaly detection failed: ${err.message}`);
-  });
 
   return newTelemetry;
 };
---
+++ api-gateaway/src/workers/aiAlerts.listener.js
@@ -0,0 +1,33 @@
+import { consumeFromQueue } from "../config/rabbitmq.js";
+import * as alertService from "../modules/alerts/alert.service.js";
+import logger from "../utils/logger.js";
+
+export const startAlertListener = async () => {
+  const queueName = process.env.AI_ALERTS_QUEUE;
+
+  await consumeFromQueue(queueName, async (msg, channel) => {
+    try {
+      const payload = JSON.parse(msg.content.toString());
+
+      if (payload.severity === "CRITICAL") {
+        await alertService.createAlert({
+          equipmentId: payload.equipmentId,
+          severity: payload.severity,
+          message: payload.message,
+        });
+        logger.info(`[WORKER] AI Alert Auto-Escalated for Equipment: ${payload.equipmentId}`);
+      }
+
+      // TODO: Broadcast ke Frontend via WebSocket
+      // global.io.emit("ai-telemetry-update", payload);
+
+      // Tell RABBITMQ the task is done
+      channel.ack(msg);
+    } catch (error) {
+      logger.error(`[WORKER] Failed processing AI Alert: ${error.message}`);
+
+      // Negative Acknowledgement so that message are returned to the queue if the database system goed down
+      channel.nack(msg, false, true);
+    }
+  });
+};
