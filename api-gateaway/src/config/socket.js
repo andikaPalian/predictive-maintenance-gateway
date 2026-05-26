c12624a8f8c3f99f471bbd649c3b9e0052446a02
Andika Palian <andika12des@gmail.com>
2026-05-26 09:50:35 +0800

    feat: integrate Socket.IO for real-time updates and add WebSocket support in AI alert listener

---
+++ api-gateaway/src/config/socket.js
@@ -0,0 +1,37 @@
+import { Server } from "socket.io";
+import logger from "../utils/logger.js";
+
+let io;
+
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.info(`[WEBSOCKET] Client/Postman connected: ${socket.id}`);
+
+    socket.on("subscribe-equipment", (equipmentId) => {
+      socket.join(`equipment-${equipmentId}`);
+      logger.info(
+        `[WEBSOCKET] Client ${socket.id} subscribed to equipment equipment-${equipmentId}`,
+      );
+    });
+
+    socket.on("disconnect", () => {
+      logger.info(`[WEBSOCKET] Client/Postman disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized. Call initSocket first.");
+  }
+  return io;
+};
